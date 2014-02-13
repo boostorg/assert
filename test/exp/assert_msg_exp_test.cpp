f5efafad1f76a7d812f2b74d9ec0dfa6a18845bf
Peter Dimov <pdimov@pdimov.com>
2014-02-13 17:47:28 +0200

    Fix failures when __FILE__ contains backslashes

--- test/exp/assert_msg_exp_test.cpp
+++ test/exp/assert_msg_exp_test.cpp
@@ -1,5 +1,5 @@
 //
-//  assert_exp_test.cpp - tests BOOST_ASSERT expansion
+//  assert_msg_exp_test.cpp - tests BOOST_ASSERT_MSG expansion
 //
 //  Copyright (c) 2014 Peter Dimov
 //
@@ -13,8 +13,23 @@
 #include <boost/detail/lightweight_test.hpp>
 #include <string>
 
+// Each backslash in __FILE__ when passed through BOOST_STRINGIZE is doubled
+static std::string quote( std::string const & s )
+{
+    std::string r;
+    r.reserve( s.size() );
+
+    for( char const * p = s.c_str(); *p; ++p )
+    {
+        r += *p;
+        if( *p == '\\' ) r += *p;
+    }
+
+    return r;
+}
+
 // default case, !NDEBUG
-// BOOST_ASSERT(x) -> assert(x)
+// BOOST_ASSERT_MSG(x,"m") -> assert((x)&&("m"))
 
 #undef NDEBUG
 #include <boost/assert.hpp>
@@ -22,12 +37,12 @@
 
 void test_default()
 {
-    std::string v1 = BOOST_STRINGIZE(BOOST_ASSERT(x1));
-    BOOST_TEST_EQ( v1, "assert(x1)" );
+    std::string v1 = BOOST_STRINGIZE(BOOST_ASSERT_MSG(x1, "m1"));
+    BOOST_TEST_EQ( v1, "assert((x1)&&(\"m1\"))" );
 }
 
 // default case, NDEBUG
-// BOOST_ASSERT(x) -> assert(x)
+// BOOST_ASSERT_MSG(x,"m") -> assert((x)&&("m"))
 
 #define NDEBUG
 #include <boost/assert.hpp>
@@ -35,12 +50,12 @@ void test_default()
 
 void test_default_ndebug()
 {
-    std::string v2 = BOOST_STRINGIZE(BOOST_ASSERT(x2));
-    BOOST_TEST_EQ( v2, "assert(x2)" );
+    std::string v2 = BOOST_STRINGIZE(BOOST_ASSERT_MSG(x2, "m2"));
+    BOOST_TEST_EQ( v2, "assert((x2)&&(\"m2\"))" );
 }
 
 // BOOST_DISABLE_ASSERTS, !NDEBUG
-// BOOST_ASSERT(x) -> ((void)0)
+// BOOST_ASSERT_MSG(x,"m") -> ((void)0)
 
 #define BOOST_DISABLE_ASSERTS
 
@@ -49,26 +64,26 @@ void test_default_ndebug()
 
 void test_disabled()
 {
-    std::string v3 = BOOST_STRINGIZE(BOOST_ASSERT(x3));
+    std::string v3 = BOOST_STRINGIZE(BOOST_ASSERT_MSG(x3, "m3"));
     BOOST_TEST_EQ( v3, "((void)0)" );
 }
 
 // BOOST_DISABLE_ASSERTS, NDEBUG
-// BOOST_ASSERT(x) -> ((void)0)
+// BOOST_ASSERT_MSG(x,"m") -> ((void)0)
 
 #define NDEBUG
 #include <boost/assert.hpp>
 
 void test_disabled_ndebug()
 {
-    std::string v4 = BOOST_STRINGIZE(BOOST_ASSERT(x4));
+    std::string v4 = BOOST_STRINGIZE(BOOST_ASSERT_MSG(x4, "m4"));
     BOOST_TEST_EQ( v4, "((void)0)" );
 }
 
 #undef BOOST_DISABLE_ASSERTS
 
 // BOOST_ENABLE_ASSERT_HANDLER, !NDEBUG
-// BOOST_ASSERT(expr) -> (BOOST_LIKELY(!!(expr))? ((void)0): ::boost::assertion_failed(#expr, BOOST_CURRENT_FUNCTION, __FILE__, __LINE__))
+// BOOST_ASSERT_MSG(expr, msg) -> (BOOST_LIKELY(!!(expr))? ((void)0): ::boost::assertion_failed_msg(#expr, msg, BOOST_CURRENT_FUNCTION, __FILE__, __LINE__))
 
 #undef BOOST_LIKELY
 #undef BOOST_CURRENT_FUNCTION
@@ -80,21 +95,21 @@ void test_disabled_ndebug()
 
 void test_handler()
 {
-    std::string v5 = BOOST_STRINGIZE(BOOST_ASSERT(x5)); std::string w5 = "(BOOST_LIKELY(!!(x5))? ((void)0): ::boost::assertion_failed(\"x5\", BOOST_CURRENT_FUNCTION, \"" __FILE__ "\", " BOOST_STRINGIZE(__LINE__) "))";
+    std::string v5 = BOOST_STRINGIZE(BOOST_ASSERT_MSG(x5, "m5")); std::string w5 = "(BOOST_LIKELY(!!(x5))? ((void)0): ::boost::assertion_failed_msg(\"x5\", \"m5\", BOOST_CURRENT_FUNCTION, \"" + quote( __FILE__ ) + "\", " BOOST_STRINGIZE(__LINE__) "))";
 
     char const * BOOST_CURRENT_FUNCTION = "void test_handler()";
     BOOST_TEST_EQ( v5, w5 );
 }
 
 // BOOST_ENABLE_ASSERT_HANDLER, NDEBUG
-// BOOST_ASSERT(expr) -> (BOOST_LIKELY(!!(expr))? ((void)0): ::boost::assertion_failed(#expr, BOOST_CURRENT_FUNCTION, __FILE__, __LINE__))
+// BOOST_ASSERT_MSG(expr, msg) -> (BOOST_LIKELY(!!(expr))? ((void)0): ::boost::assertion_failed_msg(#expr, msg, BOOST_CURRENT_FUNCTION, __FILE__, __LINE__))
 
 #define NDEBUG
 #include <boost/assert.hpp>
 
 void test_handler_ndebug()
 {
-    std::string v6 = BOOST_STRINGIZE(BOOST_ASSERT(x6)); std::string w6 = "(BOOST_LIKELY(!!(x6))? ((void)0): ::boost::assertion_failed(\"x6\", BOOST_CURRENT_FUNCTION, \"" __FILE__ "\", " BOOST_STRINGIZE(__LINE__) "))";
+    std::string v6 = BOOST_STRINGIZE(BOOST_ASSERT_MSG(x6, "m6")); std::string w6 = "(BOOST_LIKELY(!!(x6))? ((void)0): ::boost::assertion_failed_msg(\"x6\", \"m6\", BOOST_CURRENT_FUNCTION, \"" + quote( __FILE__ ) + "\", " BOOST_STRINGIZE(__LINE__) "))";
 
     char const * BOOST_CURRENT_FUNCTION = "void test_handler_ndebug()";
     BOOST_TEST_EQ( v6, w6 );
@@ -112,21 +127,21 @@ void test_handler_ndebug()
 
 void test_debug_handler()
 {
-    std::string v7 = BOOST_STRINGIZE(BOOST_ASSERT(x7)); std::string w7 = "(BOOST_LIKELY(!!(x7))? ((void)0): ::boost::assertion_failed(\"x7\", BOOST_CURRENT_FUNCTION, \"" __FILE__ "\", " BOOST_STRINGIZE(__LINE__) "))";
+    std::string v7 = BOOST_STRINGIZE(BOOST_ASSERT_MSG(x7, "m7")); std::string w7 = "(BOOST_LIKELY(!!(x7))? ((void)0): ::boost::assertion_failed_msg(\"x7\", \"m7\", BOOST_CURRENT_FUNCTION, \"" + quote( __FILE__ ) + "\", " BOOST_STRINGIZE(__LINE__) "))";
 
     char const * BOOST_CURRENT_FUNCTION = "void test_debug_handler()";
     BOOST_TEST_EQ( v7, w7 );
 }
 
 // BOOST_ENABLE_ASSERT_DEBUG_HANDLER, NDEBUG
-// BOOST_ASSERT(x) -> ((void)0)
+// BOOST_ASSERT_MSG(x,"m") -> ((void)0)
 
 #define NDEBUG
 #include <boost/assert.hpp>
 
 void test_debug_handler_ndebug()
 {
-    std::string v8 = BOOST_STRINGIZE(BOOST_ASSERT(x8));
+    std::string v8 = BOOST_STRINGIZE(BOOST_ASSERT_MSG(x8, "m8"));
 
     char const * BOOST_CURRENT_FUNCTION = "void test_debug_handler_ndebug()";
     BOOST_TEST_EQ( v8, "((void)0)" );
