aacdf31a33eb81865119815bc8be8437dde90f65
Ruben Perez <rubenperez038@gmail.com>
2026-02-26 12:18:44 +0100

    Restore source_location

--- include/boost/assert/source_location.hpp
+++ include/boost/assert/source_location.hpp
@@ -7,41 +7,24 @@
 // Distributed under the Boost Software License, Version 1.0.
 // http://www.boost.org/LICENSE_1_0.txt
 
-#if defined(BOOST_USE_MODULES) && !defined(BOOST_ASSERT_INTERFACE_UNIT)
-
-#ifndef BOOST_ASSERT_SKIP_IMPORT
-import boost.assert;
-#endif
-
-#else
-
-#ifndef BOOST_USE_MODULES
 #include <boost/config.hpp>
 #include <boost/cstdint.hpp>
-#endif
-#include <boost/config/std/string.hpp>
-#include <boost/config/std/cstdio.hpp>
-#include <boost/config/std/cstring.hpp>
+#include <string>
+#include <cstdio>
+#include <cstring>
 
 #if !defined(BOOST_NO_IOSTREAM)
-#include <boost/config/std/iosfwd.hpp>
+#include <iosfwd>
 #endif
 
 #if defined(__cpp_lib_source_location) && __cpp_lib_source_location >= 201907L
-# include <boost/config/std/source_location.hpp>
-#endif
-
-// BOOST_ASSERT_MODULE_EXPORT
-#ifdef BOOST_USE_MODULES
-#  define BOOST_ASSERT_MODULE_EXPORT export
-#else
-#  define BOOST_ASSERT_MODULE_EXPORT
+# include <source_location>
 #endif
 
 namespace boost
 {
 
-BOOST_ASSERT_MODULE_EXPORT struct source_location
+struct source_location
 {
 private:
 
@@ -154,7 +137,6 @@ public:
 
 #if !defined(BOOST_NO_IOSTREAM)
 
-BOOST_ASSERT_MODULE_EXPORT
 template<class E, class T> std::basic_ostream<E, T> & operator<<( std::basic_ostream<E, T> & os, source_location const & loc )
 {
     os << loc.to_string();
@@ -165,11 +147,58 @@ template<class E, class T> std::basic_ostream<E, T> & operator<<( std::basic_ost
 
 } // namespace boost
 
-#endif // Modules compatibility header
+#if defined(BOOST_DISABLE_CURRENT_LOCATION)
 
-// Not safe to include in the interface unit because of includes, and not needed
-#if !defined(BOOST_ASSERT_INTERFACE_UNIT)
-#include <boost/assert/current_location.hpp>
+# define BOOST_CURRENT_LOCATION ::boost::source_location()
+
+#elif defined(BOOST_MSVC) && BOOST_MSVC >= 1935
+
+# define BOOST_CURRENT_LOCATION ::boost::source_location(__builtin_FILE(), __builtin_LINE(), __builtin_FUNCSIG(), __builtin_COLUMN())
+
+#elif defined(BOOST_MSVC) && BOOST_MSVC >= 1926
+
+// std::source_location::current() is available in -std:c++20, but fails with consteval errors before 19.31, and doesn't produce
+// the correct result under 19.31, so prefer the built-ins
+# define BOOST_CURRENT_LOCATION ::boost::source_location(__builtin_FILE(), __builtin_LINE(), __builtin_FUNCTION(), __builtin_COLUMN())
+
+#elif defined(BOOST_MSVC)
+
+// __LINE__ is not a constant expression under /ZI (edit and continue) for 1925 and before
+
+# define BOOST_CURRENT_LOCATION_IMPL_1(x) BOOST_CURRENT_LOCATION_IMPL_2(x)
+# define BOOST_CURRENT_LOCATION_IMPL_2(x) (x##0 / 10)
+
+# define BOOST_CURRENT_LOCATION ::boost::source_location(__FILE__, BOOST_CURRENT_LOCATION_IMPL_1(__LINE__), "")
+
+#elif defined(__cpp_lib_source_location) && __cpp_lib_source_location >= 201907L && !defined(__NVCC__)
+
+// Under nvcc, __builtin_source_location is not constexpr
+// https://github.com/boostorg/assert/issues/32
+
+# define BOOST_CURRENT_LOCATION ::boost::source_location(::std::source_location::current())
+
+#elif defined(BOOST_CLANG) && BOOST_CLANG_VERSION >= 90000
+
+# define BOOST_CURRENT_LOCATION ::boost::source_location(__builtin_FILE(), __builtin_LINE(), __builtin_FUNCTION(), __builtin_COLUMN())
+
+#elif defined(BOOST_GCC) && BOOST_GCC >= 80000
+
+// The built-ins are available in 4.8+, but are not constant expressions until 7
+// In addition, reproducible builds require -ffile-prefix-map, which is GCC 8
+// https://github.com/boostorg/assert/issues/38
+
+# define BOOST_CURRENT_LOCATION ::boost::source_location(__builtin_FILE(), __builtin_LINE(), __builtin_FUNCTION())
+
+#elif defined(BOOST_GCC) && BOOST_GCC >= 50000
+
+// __PRETTY_FUNCTION__ is allowed outside functions under GCC, but 4.x suffers from codegen bugs
+# define BOOST_CURRENT_LOCATION ::boost::source_location(__FILE__, __LINE__, __PRETTY_FUNCTION__)
+
+#else
+
+// __func__ macros aren't allowed outside functions, but BOOST_CURRENT_LOCATION is
+# define BOOST_CURRENT_LOCATION ::boost::source_location(__FILE__, __LINE__, "")
+
 #endif
 
 #endif // #ifndef BOOST_ASSERT_SOURCE_LOCATION_HPP_INCLUDED
